a8e9705e62b2b59266e4f25f8744e89754c3d764
toshifumi1989 <rogusamu@yahoo.co.jp>
2016-05-22 17:55:15 +0900

    とりあえず終わり

--- ハノイの塔.cpp
+++ ハノイの塔.cpp
@@ -1,69 +1,67 @@
 #include<stdio.h>
 
-const int MaxDisk = 3;
+int a[] = { 1 ,2 ,3 ,4};
+int b[] = { 0 ,0 ,0 ,0};
+int c[] = { 0 ,0 ,0 ,0};
 
-int a[3] = { 1 ,2 ,3 };
-int b[3] = { 0 ,0 ,0 };
-int c[3] = { 0 ,0 ,0 };
+const int MaxDisk = 4;
 
-int diskCounter[3] = { 2,0,0 };
+void printPole()
+{
 
-void printPole() {
+	for (int i = MaxDisk - 1 ; i >= 0; i--)
+	{
+		printf(" %d %d %d\n", a[i],b[i],c[i]);
 
-	for (int i = 0; i < 3; i++) {
-		printf(" %d ", a[i]);
-
-	}
-	printf("\n");
-
-	for (int i = 0; i < 3; i++) {
-		printf(" %d ", b[i]);
-	}
-	printf("\n");
-
-	for (int i = 0; i < 3; i++) {
-		printf(" %d ", c[i]);
 	}
 	printf("\n");
 	printf("\n");
 }
 
-void move(int from[], int to[], int work[] ,int n) {
-
-	if (n == 1) {
-		to[0] = from[MaxDisk - 1];
-		from[MaxDisk - 1] = 0;
-
-		printPole();
+int diskCounter(int _pole[]) 
+{
+	int counter = 0;
+	for (int i = 0; i < MaxDisk; i++)
+	{
+		if (_pole[i] != 0)
+		{
+			counter++;
+		}
 	}
-	else {
+	//printf("%d\n", counter);
+	return counter;
+}
 
-		if (n % 2 == 0)
-		{
-			int temporary = work[diskCounter[1]];
+void move(int from[], int to[], int other[], int n)
+{
 
-			work[diskCounter[1]] = from[diskCounter[0]];
-			diskCounter[1] += 1;
 
-			from[diskCounter[0]] = temporary;
-			diskCounter[0] -= 1;
+	if (n < 1)
+	{
+		return;
+	}
+	else if (n == 1)
+	{
 
-		}
-		else
-		{
-			int temporary = to[diskCounter[2]];
-
-			to[diskCounter[2]] = from[diskCounter[0]];
-			diskCounter[2] += 1;
-
-			from[n - 1] = temporary;
-			diskCounter[0] -= 1;
-		}
-		
+		int temporary = to[diskCounter(to)];
+		to[diskCounter(to)] = from[ diskCounter(from) - 1 ];
+		from[diskCounter(from) - 1] = temporary;
 
 		printPole();
 
-		move(work, to, from, n - 1);
+	}
+	else
+	{
+
+		move(from, other, to, n - 1);
+
+		int temporary = to[diskCounter(to)];
+		to[diskCounter(to)] = from[diskCounter(from) - 1];
+		from[diskCounter(from) - 1] = temporary;
+
+		printPole();
+	
+		move(other , to , from, n - 1);
 
 	}
 
@@ -74,7 +72,7 @@ void main() {
 
 	printPole();
 
-	move(a, c, b, 3);
+	move(a, c, b, MaxDisk);
 
 	getchar();
 
